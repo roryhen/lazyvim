b6196b67c43ca477655fd6cdc82591d074216430
Rory Hendrickson <hendricksonrory@gmail.com>
2024-05-27 10:36:11 -0700

    noice options

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,19 +1,9 @@
 return {
   "folke/noice.nvim",
   opts = {
-    views = {
-      cmdline_popup = {
-        position = {
-          row = "90%",
-          col = "50%",
-        },
-      },
-      cmdline_popupmenu = {
-        position = {
-          row = "80%",
-          col = "50%",
-        },
-      },
+    presets = {
+      bottom_search = false,
+      command_palette = false,
     },
   },
 }
