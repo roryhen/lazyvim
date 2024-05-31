b67787f0ec05bd0287a83089847048af9f8033a4
Rory Hendrickson <hendricksonrory@gmail.com>
2024-05-31 13:02:40 -0700

    fix eslint config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,24 +3,12 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        eslint = {},
         denols = {
           root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc"),
         },
         graphql = {
           root_dir = require("lspconfig").util.root_pattern("codegen.ts"),
         },
-      },
-      setup = {
-        eslint = function()
-          require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "eslint" then
-              client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
-          end)
-        end,
       },
     },
   },
