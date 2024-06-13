fb78c9794d297a41d9b444125ffaf7f2c5e4f3f9
Rory Hendrickson <hendricksonrory@gmail.com>
2024-06-12 21:47:20 -0700

    back to neo-tree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,6 +1,12 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    enabled = false,
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+        },
+      },
+    },
   },
 }
