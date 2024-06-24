a88e795c101d24bc1152f98325e0b2368fb4a48c
Rory Hendrickson <hendricksonrory@gmail.com>
2024-06-23 22:32:48 -0700

    remove config for lsp servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,19 +1,9 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
     opts = {
       inlay_hints = { enabled = false },
-      servers = {
-        denols = {
-          root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc"),
-        },
-        graphql = {
-          root_dir = require("lspconfig").util.root_pattern("codegen.ts"),
-        },
-        eslint = {
-          root_dir = require("lspconfig").util.root_pattern("package.json"),
-        },
-      },
     },
   },
 }
