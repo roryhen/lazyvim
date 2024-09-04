792b4452eb56bb251c52023eeb706f176e76a41e
Rory Hendrickson <35480205+roryhen@users.noreply.github.com>
2024-09-03 20:14:26 -0700

    add neotest-vitest adapter

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,15 +1,16 @@
 return {
   {
     "nvim-neotest/neotest",
+    lazy = false,
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "nvim-neotest/neotest-jest",
+      "marilari88/neotest-vitest",
     },
     opts = {
       adapters = {
-        ["neotest-jest"] = {
-          jestCommand = "pnpm test --",
-        },
+        ["neotest-jest"] = {},
+        ["neotest-vitest"] = {},
       },
     },
   },
