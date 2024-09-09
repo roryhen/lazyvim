71da148f4abc1702e55f4e96347967a6de49976d
Rory Hendrickson <35480205+roryhen@users.noreply.github.com>
2024-09-09 08:47:20 -0700

    add window resize, update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,9 @@ set("n", "<leader>sv", "<cmd>luafile $MYVIMRC<cr>", { desc = "Source nvim config
 -- Center cursor after jumping
 set("n", "<C-u>", "<C-u>zz", { desc = "Half page up" })
 set("n", "<C-d>", "<C-d>zz", { desc = "Half page down" })
+
+-- Window resize
+set("n", "<M-,>", "<c-w>5<", { desc = "Decrease window width" })
+set("n", "<M-.>", "<c-w>5>", { desc = "Increase window width" })
+set("n", "<M-t>", "<C-W>+", { desc = "Increase window height" })
+set("n", "<M-s>", "<C-W>-", { desc = "Decrease window height" })
