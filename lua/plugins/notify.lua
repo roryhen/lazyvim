42c0008190ccc5e547b2034c2c153c8bd6aee835
Rory Hendrickson <35480205+roryhen@users.noreply.github.com>
2024-09-13 09:40:55 -0700

    terminal size

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -5,8 +5,5 @@ return {
       render = "compact",
       top_down = false,
     },
-    keys = {
-      { "<leader>sN", "<cmd>:Telescope notify<cr>", desc = "Notification history (notify)" },
-    },
   },
 }
--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -2,7 +2,7 @@ return {
   {
     "akinsho/toggleterm.nvim",
     opts = {
-      size = 25,
+      size = 32,
     },
     keys = {
       { "<C-/>", "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" },
